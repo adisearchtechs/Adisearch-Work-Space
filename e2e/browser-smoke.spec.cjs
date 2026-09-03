81bc025bc1a0173d235e47111464b812266d12cf
adisearchtechs <163436371+adisearchtechs@users.noreply.github.com>
2026-09-03 20:42:45 +0300

    R8 allow CommonJS in Playwright browser spec

--- e2e/browser-smoke.spec.cjs
+++ e2e/browser-smoke.spec.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { test, expect } = require('@playwright/test');
 
 test.describe('Adisearch Workspace browser certification baseline', () => {
