049bc00d095aa490fc86dcaae33cab4e19c67ac5
adisearchtechs <163436371+adisearchtechs@users.noreply.github.com>
2026-09-05 13:09:54 +0300

    fix(issues): route configured detail through persistent runtime

--- app/[orgId]/issue/[issueId]/page.tsx
+++ app/[orgId]/issue/[issueId]/page.tsx
@@ -1,11 +1,11 @@
-import IssueDetails from '@/components/common/issues/details/issue-details';
+import IssueDetailsRuntime from '@/components/common/issues/details/issue-details-runtime';
 import Header from '@/components/layout/headers/issue/header';
 import MainLayout from '@/components/layout/main-layout';
 
 export default function IssueDetailPage() {
    return (
       <MainLayout header={<Header />} headersNumber={1}>
-         <IssueDetails />
+         <IssueDetailsRuntime />
       </MainLayout>
    );
 }
